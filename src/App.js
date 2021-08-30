8cb91e0b7a3d65db94b9895a3d138e3b45bb1f9b
Mayen Akpan <neroxma@yahoo.com>
2021-08-29 22:28:23 -0700

    Added components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import Container from "./components/Container";
+import Title from "./components/Title";
+import Options from "./components/Options";
 
 function App() {
+  const [paragraphs, setParagraphs] = useState([]);
+  const [tag, setTag] = useState("p");
+  const [inputValue, setinputValue] = useState(1);
+  const [includeHtml, setIncludeHtml] = useState("Yes");
+  const [copiedCode, setCopiedCode] = useState(false);
+
   return (
     <div className="App">
-      <h1>Text Generator</h1>
+      <Container>
+        <Title />
+        <Options />
+      </Container>
     </div>
   );
 }
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import Container from "./Container";
+
+function Title() {
+  return (
+    <div className="title">
+      <h1>Text Generator App</h1>
+    </div>
+  );
+}
+
+export default Title;
